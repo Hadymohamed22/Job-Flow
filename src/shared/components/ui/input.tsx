c14531bfc650c2f739ebf405ef15627c52773e18
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-06-17 01:02:23 +0300

    refactor(input): enhance password show password button implementation

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -6,15 +6,18 @@ import { Eye, EyeClosed } from "lucide-react";
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   // States
-  const [isAppear, setIsAppear] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  // Variables
+  const PasswordIcon = showPassword ? EyeClosed : Eye;
 
   // Functions
-  const toggleInputType = () => setIsAppear((prev) => !prev);
+  const toggleInputType = () => setShowPassword((prev) => !prev);
 
   return (
-    <div className="input-container relative">
+    <div className="relative">
       <input
-        type={type !== "password" ? type : isAppear ? "text" : "password"}
+        type={type !== "password" ? type : showPassword ? "text" : "password"}
         data-slot="input"
         className={cn(
           "min-h-10 w-full min-w-0 rounded-lg border border-[#464554] bg-transparent px-4 py-2.5 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-gray-600 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
@@ -23,20 +26,16 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         )}
         {...props}
       />
-      {type === "password" &&
-        (isAppear ? (
-          <EyeClosed
-            className="absolute right-4 top-1/2 -translate-y-1/2 z-10"
-            onClick={toggleInputType}
-            size={16}
-          />
-        ) : (
-          <Eye
-            className="absolute right-4 top-1/2 -translate-y-1/2 z-10"
-            onClick={toggleInputType}
-            size={16}
-          />
-        ))}
+      {type === "password" && (
+        <button
+          type="button"
+          onClick={toggleInputType}
+          aria-label={showPassword ? "Hide password" : "Show password"}
+          className="absolute right-3 top-1/2 -translate-y-1/2 z-10"
+        >
+          <PasswordIcon size={16} />
+        </button>
+      )}
     </div>
   );
 }

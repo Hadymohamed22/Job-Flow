acc054678ce8a7322b972014b120284ef0eaed9b
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-06-25 22:04:13 +0300

    feat(forget-pass): create AuthFormIconContainer

---
+++ src/app/(auth)/_components/auth-form-icon-container.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AuthFormIconContainer({ children }: Props) {
+  return (
+    <div className="auth-form-icon-container size-14 md:size-16 rounded-xl border border-[#464554] flex items-center justify-center">
+      {children}
+    </div>
+  );
+}

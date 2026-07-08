c096c21c5162afe4b6d14569df0808286c5e0526
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-07-08 10:51:02 +0300

    feat(applications): implement application header UI

--- src/app/(main)/dashboard/applications/page.tsx
+++ src/app/(main)/dashboard/applications/page.tsx
@@ -1,3 +1,34 @@
+import { Button } from "@/shared/components/ui/button";
+import PageHeaderText from "../_components/page-header-text";
+import { Download } from "lucide-react";
+import StatusMenu from "./_components/status-menu";
+
 export default function Page() {
-  return <div>applications</div>;
+  return (
+    <>
+      {/* Header */}
+      <header className="applications-header flex flex-col gap-4 md:gap-0 md:flex-row md:items-end justify-between">
+        {/* Applications Page Header */}
+        <PageHeaderText
+          title="Applications"
+          subTitle="Manage and track your active job search process."
+        />
+
+        {/* Filters */}
+        <div className="actions flex md:items-center gap-2">
+          {/* Status Menu */}
+          <StatusMenu />
+
+          {/* Export Button */}
+          <Button
+            variant={"outline"}
+            className="py-2 px-4 text-[#DAE2FD] hover:text-gray-200 w-full md:w-fit"
+          >
+            <Download />
+            Export
+          </Button>
+        </div>
+      </header>
+    </>
+  );
 }
